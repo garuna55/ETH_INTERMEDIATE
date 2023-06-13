92b7ac8b0b67712cac01de503cff4d85dfeaa5cf
Anurag Rawat <75340540+garuna55@users.noreply.github.com>
2023-06-13 12:28:19 +0530

    Add files via upload

---
+++ memory_and_storage.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.5;
+contract challenge {
+    function memoryfunction(uint a, uint b)public pure returns(uint)  {
+        return a+b;
+    }
+    struct Car {
+        string carname;
+        uint milage ;
+    }
+    Car car = Car("Audi", 21);
+    Car car1 = Car("Mercedes", 23);
+    Car car2 = Car("Alto", 40);
+
+    function car_details()public view returns(string memory,uint) {
+        return( car.carname,car.milage); 
+    }
+     function car1_details()public view returns(string memory,uint) {
+        return( car1.carname,car1.milage); 
+    }
+    function car2_details()public view returns(string memory,uint) {
+        return( car2.carname,car2.milage); 
+    }
+
+    }  
+
