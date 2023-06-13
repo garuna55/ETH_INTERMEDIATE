a2c0ed535bbe84e295393275cebdc7419fbcb9f0
Anurag Rawat <75340540+garuna55@users.noreply.github.com>
2023-06-13 12:30:10 +0530

    Update memory_and_storage.sol

--- memory_and_storage.sol
+++ memory_and_storage.sol
@@ -1,9 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.5;
 contract challenge {
-    function memoryfunction(uint a, uint b)public pure returns(uint)  {
+    function addition(uint a, uint b)public pure returns(uint)  {
         return a+b;
     }
+    //additon function is declared in memory.
+
     struct Car {
         string carname;
         uint milage ;
@@ -11,6 +13,9 @@ contract challenge {
     Car car = Car("Audi", 21);
     Car car1 = Car("Mercedes", 23);
     Car car2 = Car("Alto", 40);
+
+
+    //Car structure is declared in storage.
 
     function car_details()public view returns(string memory,uint) {
         return( car.carname,car.milage); 
